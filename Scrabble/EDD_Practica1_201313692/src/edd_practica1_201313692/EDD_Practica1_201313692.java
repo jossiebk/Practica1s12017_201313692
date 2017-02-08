2c5737d9dc5bdef45696c729e85577d69fbf1767
Jossie Castrillo <bkcastrillo@gmail.com>
2017-02-07 18:51:12 -0600

    Creacion de la app
    solo cree la carpeta que contendra la aplicacion

---
+++ Scrabble/EDD_Practica1_201313692/src/edd_practica1_201313692/EDD_Practica1_201313692.java
@@ -0,0 +1,15 @@
+
+package edd_practica1_201313692;
+
+/**
+ *
+ * @author jossie
+ */
+public class EDD_Practica1_201313692 {
+
+   
+    public static void main(String[] args) {
+      
+    }
+    
+}
